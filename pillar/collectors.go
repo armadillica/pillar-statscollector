169690971ae36327878fe5469e318c8195067b03
Sybren A. Stüvel <sybren@stuvel.eu>
2017-09-08 11:22:21 +0200

    Merged pillar/{main,collectors}.go

--- pillar/collectors.go
+++ pillar/collectors.go
@@ -4,6 +4,7 @@ import (
 	"time"
 
 	"github.com/armadillica/pillar-statscollector/elastic"
+	log "github.com/sirupsen/logrus"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -21,3 +22,48 @@ type m bson.M
 var notDeletedQuery = m{"_deleted": m{"$ne": true}}
 
 // collector methods are defined in the collector_xxx.go files.
+
+// CollectStats collects all the statistics and returns it as elastic.Stats object.
+func CollectStats(session *mgo.Session) (elastic.Stats, error) {
+	now := time.Now().UTC()
+	stats := elastic.Stats{
+		SchemaVersion: 1,
+		Timestamp:     now,
+	}
+
+	c := collector{
+		now,
+		&stats,
+		session.DB("").C("files"),
+		session.DB("").C("projects"),
+		session.DB("").C("nodes"),
+	}
+
+	if err := c.filesTotalCount(); err != nil {
+		return stats, err
+	}
+	if err := c.filesExpiredLinks(); err != nil {
+		return stats, err
+	}
+	if err := c.filesEmptyLinks(); err != nil {
+		return stats, err
+	}
+	if err := c.filesCountStatsPerStorageBackend(); err != nil {
+		return stats, err
+	}
+	if err := c.filesCountStatsPerStatus(); err != nil {
+		return stats, err
+	}
+
+	if err := c.projectsCount(); err != nil {
+		return stats, err
+	}
+
+	if err := c.nodesCount(); err != nil {
+		return stats, err
+	}
+
+	// Done!
+	log.Info("Done collecting statistics")
+	return stats, nil
+}
